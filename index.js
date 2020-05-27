d2148f608f2417e096d0a3a37de6f087470ae7aa
Girma Nigusse <xgirma@gmail.com>
2020-05-26 20:41:23 -0400

    updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const gp = require('gh-pages');
 
-gp.publish('api', function(error) {
-    console.error(error);
+gp.publish('api', function (error) {
+    if (error) {
+        console.error(error);
+    }
 });
