a5bd6f3b841490bc533bd604c60ff7ed87f6e83e
Girma Nigusse <xgirma@gmail.com>
2020-06-11 19:23:48 -0400

    updates

---
+++ src/puppeteer.js
@@ -0,0 +1,255 @@
+const { writeFileSync } = require("fs");
+const { stringify, sort } = require("../lib/common");
+const { ASSERT, EXPECT, SHOULD } = require("./common/assertion");
+const {
+  CUCUMBER,
+  JASMINE,
+  MOCHA,
+  CHAI,
+  AVA,
+  JEST,
+} = require("./common/runner");
+const { JAVASCRIPT, ESMODULES, TYPESCRIPT } = require("./common/javascript");
+const { BABEL, ESM, TSNODE, TSJEST } = require("./common/translator");
+
+const BASE = "./api/v1/platforms/nodejs/puppeteer/";
+
+(() => {
+  const data = stringify(sort([JAVASCRIPT, ESMODULES, TYPESCRIPT]));
+  writeFileSync(`${BASE}index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([CUCUMBER, JASMINE, MOCHA]));
+  writeFileSync(`${BASE}commonjs/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, CHAI]));
+  writeFileSync(`${BASE}commonjs/cucumber/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}commonjs/cucumber/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT, SHOULD]));
+  writeFileSync(`${BASE}commonjs/cucumber/chai/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(
+    `${BASE}commonjs/cucumber/chai/assert/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(
+    `${BASE}commonjs/cucumber/chai/expect/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([SHOULD]));
+  writeFileSync(
+    `${BASE}commonjs/cucumber/chai/should/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT]));
+  writeFileSync(`${BASE}commonjs/jasmine/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}commonjs/jasmine/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(`${BASE}commonjs/jasmine/expect/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, CHAI]));
+  writeFileSync(`${BASE}commonjs/mocha/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}commonjs/mocha/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT, SHOULD]));
+  writeFileSync(`${BASE}commonjs/mocha/chai/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}commonjs/mocha/chai/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(`${BASE}commonjs/mocha/chai/expect/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([SHOULD]));
+  writeFileSync(`${BASE}commonjs/mocha/chai/should/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([BABEL, ESM]));
+  writeFileSync(`${BASE}es-modules/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([BABEL, ESM]));
+  writeFileSync(`${BASE}es-modules/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([AVA, JEST]));
+  writeFileSync(`${BASE}es-modules/babel/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([AVA]));
+  writeFileSync(`${BASE}es-modules/babel/ava/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT]));
+  writeFileSync(`${BASE}es-modules/babel/jest/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}es-modules/babel/jest/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(`${BASE}es-modules/babel/jest/expect/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([MOCHA]));
+  writeFileSync(`${BASE}es-modules/esm/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, CHAI]));
+  writeFileSync(`${BASE}es-modules/esm/mocha/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(`${BASE}es-modules/esm/mocha/assert/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT, SHOULD]));
+  writeFileSync(`${BASE}es-modules/esm/mocha/chai/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(
+    `${BASE}es-modules/esm/mocha/chai/assert/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(
+    `${BASE}es-modules/esm/mocha/chai/expect/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([SHOULD]));
+  writeFileSync(
+    `${BASE}es-modules/esm/mocha/chai/should/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([TSNODE, TSJEST]));
+  writeFileSync(`${BASE}typescript/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([JEST]));
+  writeFileSync(`${BASE}typescript/ts-jest/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT]));
+  writeFileSync(`${BASE}typescript/ts-jest/jest/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(
+    `${BASE}typescript/ts-jest/jest/assert/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(
+    `${BASE}typescript/ts-jest/jest/expect/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([JASMINE]));
+  writeFileSync(`${BASE}typescript/ts-node/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT, EXPECT]));
+  writeFileSync(`${BASE}typescript/ts-node/jasmine/index.json`, data, "utf8");
+})();
+
+(() => {
+  const data = stringify(sort([ASSERT]));
+  writeFileSync(
+    `${BASE}typescript/ts-node/jasmine/assert/index.json`,
+    data,
+    "utf8"
+  );
+})();
+
+(() => {
+  const data = stringify(sort([EXPECT]));
+  writeFileSync(
+    `${BASE}typescript/ts-node/jasmine/expect/index.json`,
+    data,
+    "utf8"
+  );
+})();
