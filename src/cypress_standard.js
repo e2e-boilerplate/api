eef50c0f85dea32e1b14d5d04e13ca9e20184b76
Girma Nigusse <xgirma@gmail.com>
2020-06-22 23:27:48 -0400

    updates

--- src/cypress_standard.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require("fs");
-const { stringify, sort } = require("../lib/common");
-
-const BASE = "./api/v1/land/browser/framework/cypress/standard/";
-
-(() => {
-  const data = stringify(sort([]));
-  writeFileSync(`${BASE}index.json`, data, "utf8");
-})();
