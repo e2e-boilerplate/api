55eea75853b1e4d5ee8f964b70526bc3e7c97e67
Girma Nigusse <xgirma@gmail.com>
2020-06-24 20:19:45 -0400

    updates

--- src/land.js
+++
@@ -1,9 +0,0 @@
-const { writeFileSync } = require("fs");
-
-const { stringify, sort } = require("../lib/common");
-const { BROWSER, NODEJS } = require("./common/platforms");
-
-(() => {
-  const data = stringify(sort([BROWSER, NODEJS]));
-  writeFileSync("./api/v1/land/index.json", data, "utf8");
-})();
