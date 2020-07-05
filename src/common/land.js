b1a9f297b0ebe4fcc8b89ec8995d357a6b3a55ef
Girma Nigusse <xgirma@gmail.com>
2020-07-05 15:02:12 -0400

    updates

--- src/common/land.js
+++ src/common/land.js
@@ -2,9 +2,8 @@ const BROWSER = {
   id: "browser",
   title: "Browser",
   subtitle: "A Web browser",
-  summary: "Run your inside a Browser",
-  description:
-    "Like unit tests, an end-to-end test could run completely inside a browser environment. Hence, debugging and related activities may require the use of the browser’s developer tools, such as the browser console.",
+  summary: "Run test inside a Browser",
+  description: "Like conventional unit tests, a JavaScript end-to-end test could run completely inside a browser environment. Debugging may require the use of the browser’s developer tools, such as the browser console. ",
   link: "https://en.wikipedia.org/wiki/Web_browser",
   domain: "wikipedia.org",
   maintained: "",
@@ -15,9 +14,8 @@ const NODEJS = {
   id: "nodejs",
   title: "Node.js",
   subtitle: "A JavaScript runtime",
-  summary: "Run your outside a browser, using Node.js runtime",
-  description:
-    "Node.js runs JavaScript outside the browser environment. When using Node.js, for end-to-end automation, except for the controlled browser, everything else runs within the Node.js runtime, outside the browser.",
+  summary: "Run test using Node.js runtime",
+  description: "Node.js runs JavaScript outside the browser environment. When using Node.js, for end-to-end automation, except for the controlled browser, everything else runs within the Node.js runtime, outside the browser.",
   link: "https://nodejs.org/en/",
   domain: "nodejs.org",
   maintained: "OpenJS Foundation",
